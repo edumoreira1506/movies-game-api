98c4e548e4b2af23f9a72d95acef36d36b35510b
Eduardo Moreira <contato@eduardoem.com.br>
2020-03-11 17:08:16 -0300

    Setup active_admin routes and user

--- config/application.rb
+++ config/application.rb
@@ -7,5 +7,13 @@ Bundler.require(*Rails.groups)
 module MoviesGameApi
   class Application < Rails::Application
     config.load_defaults 6.0
+
+    # For more information about Locale Files organization, look at:
+    # https://guides.rubyonrails.org/i18n.html#organization-of-locale-files
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    # Permitted locales available for the application
+    I18n.available_locales = [:'pt-BR']
+    # Set default locale to something other than :en
+    I18n.default_locale = :'pt-BR'
   end
 end
