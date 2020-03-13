7a7eafde70834cd535b1d4e2fdd8355df42866de
Eduardo Moreira <contato@eduardoem.com.br>
2020-03-13 15:19:50 -0300

    Add route to post ranking user

--- app/controllers/api/ranking_controller.rb
+++ app/controllers/api/ranking_controller.rb
@@ -1,7 +1,14 @@
-class Api::RankingController < ApplicationController
+class Api::RankingController < Api::ApiController
   def index
     ranking = RankingUser.sorted
 
     render json: { ok: true, ranking: ranking }
+  end
+
+  def create
+    ranking_user = RankingUser.new(name: params[:name], points: params[:points])
+    ranking_user.save!
+
+    render json: { ok: true, ranking_user: ranking_user }
   end
 end
